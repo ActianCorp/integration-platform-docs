279ec36b9c61c2af7333926b187a7d9de3461f51
Douglas Lloyd <douglas.lloyd@actian.com>
2024-01-05 09:48:41 -0500

    Try different API key

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,30 +88,22 @@ const config = {
       algolia: {
         // The application ID provided by Algolia
         appId: 'DJTQI8DCNE',
-  
         // Public API key: it is safe to commit it
-        apiKey: '557417026f266de1c4a0a43fc919ca41',
-  
+        apiKey: 'ddf3540c6bc95c1e6ea40510987bad90',//557417026f266de1c4a0a43fc919ca41 
         indexName: 'integration-platform-netlify',
-  
         // Optional: see doc section below
         contextualSearch: true,
-  
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         //externalUrlRegex: 'external\\.com|domain\\.com',
-  
         // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
         //replaceSearchResultPathname: {
         //  from: '/docs/', // or as RegExp: /\/docs\//
         //  to: '/',
         //},
-  
         // Optional: Algolia search parameters
         searchParameters: {},
-  
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
-  
         //... other Algolia params
       },
       announcementBar: {
