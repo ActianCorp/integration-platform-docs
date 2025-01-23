87f217f7679813783ae12ce94d4127091153e0ad
Douglas Lloyd <douglas.lloyd@actian.com>
2025-01-23 11:58:48 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--secondary button--md"
             to="/docs/intro">
             View the Docs &#8250;&#8250;
           </Link>
