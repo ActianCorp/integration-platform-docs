9aec9721f4b8a98f4cf1eca1687a6f3ced0ec87f
Douglas Lloyd <douglas.lloyd@actian.com>
2025-11-12 12:45:34 -0500

    testing redirects

---
+++ src/theme/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NotFound from '@theme-original/NotFound';
+
+export default function NotFoundWrapper(props) {
+  return (
+    <>
+    <h1>QQQQQQQQQ</h1>
+      
+    </>
+  );
+}
