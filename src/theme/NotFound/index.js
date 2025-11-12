7eb7328a598221f080843117dc1696ed7fcebc86
Douglas Lloyd <douglas.lloyd@actian.com>
2025-11-12 16:21:22 -0500

    Update index.js

--- src/theme/NotFound/index.js
+++ src/theme/NotFound/index.js
@@ -5,6 +5,7 @@ import Heading from '@theme/Heading';
 export default function NotFoundContent({className}) {
   return (
     <main className={clsx('container margin-vert--xl', className)}>
+      <h1>Actian Integration Platform</h1>
       <h2>Actian Integration Platform documentation has moved.</h2>
       <p> Be sure to update any bookmarks to the new URL: <a href="https://im.dev.actiandatacloud.com/guide/">https://im.dev.actiandatacloud.com/guide/</a></p>
     </main>
