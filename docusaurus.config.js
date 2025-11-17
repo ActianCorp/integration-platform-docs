a7df88a671cab8ccdb359463641842d0cac448bd
Douglas Lloyd <douglas.lloyd@actian.com>
2025-11-17 09:43:29 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ActianCorp/integration-platform-docs/tree/suggestions',
+            'https://github.com/ActianCorp/integration-platform-docs/tree/cortex',
           showLastUpdateTime: true,
          },
         theme: {
